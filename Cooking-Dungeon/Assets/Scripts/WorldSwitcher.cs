9860eb057ed2d18f11c169ffe9f09c266caca9c6
Jconrad15 <83470418+Jconrad15@users.noreply.github.com>
2023-04-02 10:32:48 -0700

    Add no switching zones

--- Cooking-Dungeon/Assets/Scripts/WorldSwitcher.cs
+++ Cooking-Dungeon/Assets/Scripts/WorldSwitcher.cs
@@ -36,6 +36,13 @@ public class WorldSwitcher : MonoBehaviour
 
     private void SwitchWorlds()
     {
+        bool canSwitch = CanSwitchHere();
+        if (canSwitch == false)
+        {
+            Debug.Log("Cannot switch here");
+            return;
+        }
+
         if (isSafeWorld)
         {
             dangerWorld.SetActive(true);
@@ -49,6 +56,30 @@ public class WorldSwitcher : MonoBehaviour
 
         // Switch boolean
         isSafeWorld = !isSafeWorld;
+    }
+
+    private bool CanSwitchHere()
+    {
+        Vector3 currentLocation = transform.position;
+        currentLocation.y += 0.5f;
+        float radius = 0.2f;
+
+        int maxColliders = 2;
+        Collider[] hitColliders = new Collider[maxColliders];
+        int numColliders = Physics.OverlapSphereNonAlloc(
+            currentLocation, radius, hitColliders);
+        for (int i = 0; i < numColliders; i++)
+        {
+            // Check if overlaping collider 
+            hitColliders[i].TryGetComponent(out NoSwitchZone noSwitchZone);
+            if (noSwitchZone != null)
+            {
+                // A no switch zone is present
+                return false;
+            }
+        }
+
+        return true;
     }
 
 }
