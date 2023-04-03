ef2ce7ddeb45a855912e3af04c3a7006830b50f8
Jconrad15 <83470418+Jconrad15@users.noreply.github.com>
2023-04-02 18:31:00 -0700

    Add audioclip calls to player movement

---
+++ Cooking-Dungeon/Assets/Scripts/SoundController.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(AudioSource))]
+public class SoundController : MonoBehaviour
+{
+    [SerializeField]
+    private AudioClip move;
+    [SerializeField]
+    private AudioClip flip;
+    [SerializeField]
+    private AudioClip rotate;
+
+    private AudioSource audioSource;
+
+    private void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+
+        // Register to callbacks
+        PlayerController pc = FindAnyObjectByType<PlayerController>();
+        pc.RegisterOnMove(OnMove);
+        pc.RegisterOnFlip(OnFlip);
+        pc.RegisterOnRotate(OnRotate);
+    }
+
+    private void OnMove()
+    {
+        audioSource.PlayOneShot(move);
+    }
+
+    private void OnFlip()
+    {
+        audioSource.PlayOneShot(flip);
+    }
+
+    private void OnRotate()
+    {
+        audioSource.PlayOneShot(rotate);
+    }
+}
